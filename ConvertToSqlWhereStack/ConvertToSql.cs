7007b3659c3f5d02f442ccbd458b1b8872d47113
DESKTOP-K48D6IA\Kuei <ragnakuei@livemail.tw>
2017-02-12 10:42:36 +0800

    1st green light

--- ConvertToSqlWhereStack/ConvertToSql.cs
+++ ConvertToSqlWhereStack/ConvertToSql.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace ConvertToSqlWhereStack
 {
@@ -6,11 +9,77 @@ namespace ConvertToSqlWhereStack
     {
         public ConvertToSql()
         {
+            _delimitersString = _delimiters.Select(c => c.ToString()).ToList();
         }
 
         internal string Result(string input)
         {
-            throw new NotImplementedException();
+            var inputGroupBy = ToGroupBy(input);
+            var inputPostfix = InfixToPostfix(inputGroupBy);
+            var result = PostfixToResult(inputPostfix);
+            return result;
         }
+
+        private string PostfixToResult(Queue<string> inputPostfix)
+        {
+            var fields = new Stack<string>();
+            StringBuilder result = new StringBuilder();
+            while (inputPostfix.Count > 0)
+            {
+                var next = inputPostfix.Peek();
+                switch (next)
+                {
+                    case "equals":
+                        var second = fields.Pop();
+                        var first = fields.Pop();
+                        result.Append($"{first} = {second}");
+                        inputPostfix.Dequeue();
+                        break;
+                    default:
+                        fields.Push(inputPostfix.Dequeue());
+                        break;
+                }
+            }
+            return "where "+result.ToString();
+        }
+
+        private Queue<string> ToGroupBy(string input)
+        {
+            char[] inputDelimiters = { ':', '(', ')' };
+            var inputByList = input.Split(inputDelimiters);
+            var result = new Queue<string>(inputByList);
+            return result;
+        }
+
+        private Queue<string> InfixToPostfix(Queue<string> input)
+        {
+            var result = new Queue<string>();
+
+            var operand = string.Empty;
+            while (input.Count > 0)
+            {
+                var next = input.Peek();
+                switch (next)
+                {
+                    case "equals":
+                        operand = "equals";
+                        input.Dequeue();
+                        break;
+
+                    default:
+                        if (next != string.Empty)
+                            result.Enqueue(input.Dequeue());
+                        else
+                            input.Dequeue();
+                        break;
+                }
+            }
+            result.Enqueue(operand);
+
+            return result;
+        }
+
+        private List<char> _delimiters = new List<char> { ':', '(', ')' };
+        private List<string> _delimitersString = new List<string>();
     }
 }
